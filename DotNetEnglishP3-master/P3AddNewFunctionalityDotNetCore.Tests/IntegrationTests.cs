78a5b6dd00c0edd7d28b701281f3bcafa4a0e85e
2E4O8T <benoit.froidevan@axa.fr>
2023-03-15 12:14:59 +0100

    FinalTests

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/IntegrationTests.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore.Tests/IntegrationTests.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Configuration;
 using P3AddNewFunctionalityDotNetCore.Data;
 using P3AddNewFunctionalityDotNetCore.Controllers;
 using P3AddNewFunctionalityDotNetCore.Models;
-using P3AddNewFunctionalityDotNetCore.Models.Entities;
 using P3AddNewFunctionalityDotNetCore.Models.Repositories;
 using P3AddNewFunctionalityDotNetCore.Models.Services;
 using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
 using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
@@ -23,8 +23,7 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
         public void SaveNewProductToDbTest()
         {
             var options = new DbContextOptionsBuilder<P3Referential>()
-                //.UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .UseSqlServer("Server=.;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
                 .Options;
 
             var ctx = new P3Referential(options, _configuration);
@@ -34,75 +33,51 @@ namespace P3AddNewFunctionalityDotNetCore.Tests
             var orderRepository = new OrderRepository(ctx);
             var languageService = new LanguageService();
 
-            ProductController productController = null;
+            var productController = new ProductController(new ProductService(cart, productRepository, orderRepository, _localizer), languageService);
 
-            productController = new ProductController(new ProductService(cart, productRepository, orderRepository, _localizer), languageService);
-
-            Assert.NotNull(productController);
-        }
-
-        [Fact]
-        public void GetProductByIdTest()
-        {
-            var cart = new Models.Cart();
-            var options = new DbContextOptionsBuilder<P3Referential>()
-                //.UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
-                .Options;
-            ProductService productService = null;
-            Product product = null;
-
-            using (var ctx = new P3Referential(options, _configuration))
+            var newProduct = new ProductViewModel()
             {
-                productService = new ProductService(cart,
-                    new ProductRepository(ctx),
-                    new OrderRepository(ctx), _localizer);
-                product = productService.GetProductById(1);
-            }
-
-            Assert.NotNull(product);
-            Assert.Equal(1, product.Id);
-        }
-
-        [Fact]
-        public void DeleteProductFromDbAndRemoveFromCartTest()
-        {
-            var cart = new Models.Cart();
-            var options = new DbContextOptionsBuilder<P3Referential>()
-                //.UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
-                .Options;
-            ProductService productService = null;
-            ProductViewModel newProduct = new ProductViewModel
-            {
+                Id = 1000,
                 Name = "UnDroleDeNom",
                 Price = "1000",
                 Description = "UneDroleDeChose",
                 Stock = "1",
                 Details = "VraimentDrole"
+
             };
+            
+            var expected = productController.Create(newProduct) as ViewResult;
 
-            var product = new Product();
+            // Assert
+            Assert.Null(expected);
+        }
 
-            using (var ctx = new P3Referential(options, _configuration))
-            {
-                productService = new ProductService(cart,
-                    new ProductRepository(ctx),
-                    new OrderRepository(ctx), _localizer);
-                productService.SaveProduct(newProduct);
+        [Fact]
+        public void DeleteNewProductTest()
+        {
+            var options = new DbContextOptionsBuilder<P3Referential>()
+                .UseSqlServer("Server=.;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .Options;
 
-                product = ctx.Product.FirstOrDefault(p => p.Name == "UnDroleDeNom");
-                Assert.NotNull(product);
+            var ctx = new P3Referential(options, _configuration);
 
-                cart.AddItem(product, 3);
-                Assert.NotEmpty(cart.Lines);
+            var cart = new Cart();
+            var productRepository = new ProductRepository(ctx);
+            var orderRepository = new OrderRepository(ctx);
+            var languageService = new LanguageService();
 
-                productService.DeleteProduct(product.Id);
-                product = productService.GetProductById(product.Id);
-            }
+            ProductController productController = new ProductController(new ProductService(cart, productRepository, orderRepository, _localizer), languageService);
 
-            Assert.Null(product);
-            Assert.Empty(cart.Lines);
+            ProductService productService = new ProductService(null, productRepository, null, null);
+
+            var totalProduct = productService.GetAllProducts();
+
+            var lastId = totalProduct.Max(p => p.Id);
+
+            productController.DeleteProduct(lastId);
+
+            // Assert
+            Assert.DoesNotContain(productRepository.GetAllProducts(), p => p.Id == lastId);
         }
     }
 }
